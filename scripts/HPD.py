010ec48c28cec22c2af58282afe9cbb02a30e189
Drew Ames <andrew.ames@cfa.harvard.edu>
2018-05-04 10:22:33 -0400

    cleanup scripts a bit

--- scripts/HPD.py
+++ scripts/HPD.py
@@ -5,6 +5,7 @@ from scipy.special import erf
 
 
 def fit_func(d, A, b, x0, y0):
+    """Function for HPD data to be fit to-- integral of a Gaussian"""
     return A*(1-np.exp(-(d/2-x0)**2/b)) + y0
 
 def find_HPD(x, y):
@@ -34,20 +35,12 @@ def find_HPD(x, y):
     plt.ylabel('Counts')
     plt.plot(xdata, ydata, 'o', label='Measured')
     xvals = np.linspace(0, 8, 1000)
-    yvals = fit_func(xvals, A, b, x0, y0)
+    yvals = fit_func(xvals, A, b, x0, y0) 
     plt.plot(xvals, yvals, label='Fit')
     #plt.plot(xvals, fit_func(xvals, g[0], g[1], g[2], g[3]), label='Guess')
     plt.plot([0,8], [half_pow, half_pow], c='r')
-    plt.text(HPD+0.25, half_pow, 'HPD = {0:0.3f} mm'.format(HPD), va='bottom', ha='left',
-             color='r')
-    #yloc=0
-    #for (i, x) in enumerate(xvals):
-        #if x>= 0.387:
-            #yloc = yvals[i]
-            #break
-    #plt.plot([0,8], [yloc, yloc], c='r')
-    #plt.text(0.387+0.25, yloc, '387 microns, {0}% flux'.format(int(100*yloc/(half_pow*2))),
-        #va='bottom', ha='left', color='r')
+    plt.text(HPD+0.25, half_pow, 'HPD = {0:0.3f} mm'.format(HPD), va='bottom',
+        ha='left', color='r')
     
     plt.legend(loc='lower right')
     plt.xlim(0,6)
--- scripts/reflectivity.py
+++ scripts/reflectivity.py
@@ -1,70 +1,112 @@
+#! /home/bladmin/blcontrol/venv/bin/python
+
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+sys.path.append('/home/bladmin/blcontrol/scripts')
 from fwhm import cen_fwhm
 
-datadir = '/home/bladmin/SiW_for_LLNL/optic_data/SiW69_NFM1C_R11_RR305/beamline_data/19Dec2016'
-if datadir[-1] is not '/':
-    datadir += '/'
-samplename = 'RR301'
-date = '22Dec2016'
-bg_filename = 'nooptic_Alfilter.txt'
-opt_filename = 'optic_Alfilter.txt'
-line_energy = 22.16  #keV
-line_name = "Ag K alpha"
+#### USAGE: Copy this script into the directory where your beamline data is
+#### To run:
+#### ./reflectivity.py
 
-bg = np.loadtxt(datadir + bg_filename)
-opt = np.loadtxt(datadir + opt_filename)
-
-bg_pin = 5 #diameter, mm
-bg_area = np.pi * (bg_pin/2.)**2
-opt_area = 18.5 #mm^2
-
-d_opt = 600  #mm
-d_detector = 3000  #mm
-
-correction = bg_area*d_opt**2/(opt_area*d_detector**2)
-
-R_sq = np.zeros_like(bg)
-R_sq[:,0] = bg[:,0]
-R_sq[:,1] = np.nan_to_num(correction*np.divide(opt[:,1], bg[:,1]))
-
-refl = np.zeros_like(R_sq)
-refl[:,0] = R_sq[:,0]
-refl[:,1] = np.sqrt(R_sq[:,1])
-
-np.savetxt(datadir + 'reflectivity_Alfilter.txt', refl)
-refl = np.loadtxt(datadir + 'reflectivity_Alfilter.txt')
+#### Geometric Parameters ####
+D_OPT = 677.8   # distance from src to optic, mm
+D_DET = 3050    # distance from src to detector, mm
+PIN_SIZE = 3    # pinhole radius used to take src spectrum
+OPT_AREA = 46.7 # collecting area of optic in mm**2
 
 
-cen, fwhm = cen_fwhm(refl[:,0], refl[:,1])
-max_refl = max(refl[:,1])
-print 'Cen = {0:0.2f} keV\nFWHM = {1:0.2f} keV'.format(cen, fwhm)
-
-plt.figure(1)
-plt.clf()
-plt.axvline(x=line_energy, ymin=0, ymax=1, color='g', lw=2, label=line_name +
-	', ' + str(line_energy) + ' keV\nR = 0.15')
-plt.plot(refl[:,0], refl[:,1], color='b', label='Measured Reflectivity')
-plt.plot([cen-fwhm/2., cen+fwhm/2.], [max_refl/2, max_refl/2],
-    color='r')
-plt.text(cen-fwhm, max_refl/2,
-    'FWHM = {0:0.2f} keV\n     @ {1:0.2f} keV'.format(fwhm, cen), color='r', ha='right')
-#plt.text(cen, max_refl,
-         #"Max reflectivity = {0:0.2f}".format(max_refl), color='r',
-         #va='bottom')
-plt.xlim(8,30)
-plt.ylim(0,0.25)
-plt.xlabel('Energy (keV)')
-plt.ylabel('Reflectivity')
-#plt.title(samplename + ' reflectivity with 1mm Al filter, ' + date)
-plt.legend(loc='upper left')
-plt.grid()
-plt.savefig(datadir + 'refl_Alfilter.png')
-#plt.show()
+#### Data Locations ####
+# Must give absolute path to files if data is not in the same directory as this
+# script
+OPT_FILENAME = 'optic.txt'          # filename of optic spectrum
+SRC_FILENAME = 'nooptic.txt'        # filename of src spectrum (no optic)
+REFL_FILENAME = 'reflectivity.txt'  # where you want the calculated reflectivity
+                                    # to be saved
 
 
-plt.title(samplename + ' raw data, ' + date)
-plt.xlabel('Energy (keV)')
-plt.ylabel('Counts')
-plt.grid()
-plt.savefig(datadir + 'raw_Alfilter.png')
+#### Plot Parameters ####
+TITLE = 'optic name + date' # plot title
+REF_LINE = 22.16            # location of reference line to be plotted, in keV
+                            # if you don't want one, change to REF_LINE = None
+PLOT_START_ENERGY = 8       # where to start the x-axis (keV)
+PLOT_END_ENERGY = 30        # where to end the x-axis (kev)                            
+PLOT_FILENAME = 'refl.png'  # location where you want plot to be saved
+
+
+#### Options ####
+# the following line creates the reflectivity text file-- change to False
+# if the file already exists
+CREATE_TXT_FILE = True
+
+# the following line creates the plot-- change to False if you don't want a plot
+CREATE_PLOT     = True
+
+def calculate_reflectivity():
+    """Calculates the reflectivity using global variables defined above and
+    saves result to a text file."""
+    
+    # load data
+    src_spec = np.loadtxt(SRC_FILENAME)
+    opt_spec = np.loadtxt(OPT_FILENAME)
+
+    # collecting area of detector when source spectrum was taken with pinhole
+    src_pin_area = np.pi*(PIN_SIZE/2.)**2
+
+    # correction factor for source-to-optic distance & collecting area
+    # (see analysis section of manual for more explanation)
+    correction = src_pin_area*D_OPT**2/(OPT_AREA*D_DET**2)
+
+    # calculate R**2 with correction factor
+    R_sq = np.zeros_like(src_spec)
+    R_sq[:,0] = src_spec[:,0]
+    # ignore div by 0 warning for now
+    with np.errstate(divide='ignore', invalid='ignore'):
+        R_sq[:,1] = np.nan_to_num(correction*np.divide(opt_spec[:,1],
+                                                        src_spec[:,1]))
+
+    # take square root to get single-bounce reflectivity
+    refl = np.zeros_like(R_sq)
+    refl[:,0] = R_sq[:,0]
+    refl[:,1] = np.sqrt(R_sq[:,1])
+
+    # save to file
+    np.savetxt(REFL_FILENAME, refl)
+
+def plot_reflectivity():
+    """Plots single bounce reflectivity from file using parameters defined
+    above."""
+    # load reflectivity file
+    refl = np.loadtxt(REFL_FILENAME)
+
+    # calculate peak and fwhm
+    cen, fwhm = cen_fwhm(refl[:,0], refl[:,1])
+    max_refl = max(refl[:,1])
+
+    # plotting
+    plt.figure(1)
+    plt.clf()
+    if REF_LINE:
+        plt.axvline(x=REF_LINE, ymin=0, ymax=1, color='g', lw=2,
+            label=str(REF_LINE))
+    plt.plot(refl[:,0], refl[:,1], color='b', label='Measured Reflectivity')
+    plt.plot([cen-fwhm/2., cen+fwhm/2.], [max_refl/2, max_refl/2],
+        color='r')
+    fwhm_label = 'FWHM = {0:0.2f} keV\n     @ {1:0.2f} keV'.format(fwhm, cen)
+    plt.text(cen-fwhm, max_refl/2, fwhm_label, color='r', ha='right')
+    plt.xlim(PLOT_START_ENERGY, PLOT_END_ENERGY)
+    plt.xlabel('Energy (keV)')
+    plt.ylabel('Reflectivity')
+    plt.title(TITLE)
+    plt.legend(loc='upper left')
+    plt.grid()
+    plt.savefig(PLOT_FILENAME)
+
+if __name__ == "__main__":
+    if CREATE_TXT_FILE:
+        calculate_reflectivity()
+
+    if CREATE_PLOT:
+        plot_reflectivity()
+    
